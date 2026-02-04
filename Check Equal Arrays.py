f325bcbf1bc6c787c8e0964911872a2fa53e7a7f
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2026-02-04 16:46:07 +0300

    Implement checkEqual method to compare two arrays

---
+++ Check Equal Arrays.py
@@ -0,0 +1,9 @@
+class Solution:
+    def checkEqual(self, a, b) -> bool:
+        #code here
+        a.sort()
+        b.sort()
+        if a == b:
+            return True
+        else:
+            return False
