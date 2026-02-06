7ceeb9e839719fb38684d4521f0620419a6c58ba
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2026-02-06 03:56:21 +0300

    Optimize missing number detection using set

--- Missing Number.py
+++ Missing Number.py
@@ -1,8 +1,18 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        nums.sort()
+        numss = set(nums)
         for i in range(len(nums)):
-            if nums[i] != i:
+            if i not in numss:
                 return i
         return len(nums)
+
+
+
+# class Solution:
+#     def missingNumber(self, nums: List[int]) -> int:
+#         nums.sort()
+#         for i in range(len(nums)):
+#             if nums[i] != i:
+#                 return i
+#         return len(nums)
             
