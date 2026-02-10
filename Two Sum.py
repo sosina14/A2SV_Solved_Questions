f4d1c087b0189e94e40852f8878ff7e5c18db459
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2026-02-10 07:20:01 +0300

    Refactor twoSum to use a dictionary for lookup

--- Two Sum.py
+++ Two Sum.py
@@ -1,23 +1,41 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        arr = nums.copy()
-        arr.sort()
-        i = 0
-        j = len(nums)-1
-        while i < j:
-            s = arr[i] + arr[j]
-            if s == target:
-                break
-            elif s > target:
-                j -= 1
-            else:
-                i += 1
+        dic = {}
+        for i in range(len(nums)):
+            dic[nums[i]] = i
 
-        ans = []
-        for index in range(len(nums)):
-            if nums[index] == arr[i] or nums[index] == arr[j]:
-                ans.append(index)
-        return ans
+        for i in range(len(nums)):
+            x = target - nums[i]
+
+            if x in dic and dic[x] != i:
+                return[i,dic[x]]
+
+        return []
+
+        
+            
+
+
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         arr = nums.copy()
+#         arr.sort()
+#         i = 0
+#         j = len(nums)-1
+#         while i < j:
+#             s = arr[i] + arr[j]
+#             if s == target:
+#                 break
+#             elif s > target:
+#                 j -= 1
+#             else:
+#                 i += 1
+
+#         ans = []
+#         for index in range(len(nums)):
+#             if nums[index] == arr[i] or nums[index] == arr[j]:
+#                 ans.append(index)
+#         return ans
 
         
             
