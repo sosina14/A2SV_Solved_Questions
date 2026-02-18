91112a01ef85e0c9198c5a6306efe77b39f0588e
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2026-02-18 21:59:06 +0300

    Implement largestNumber function to form largest number

---
+++ 179. Largest Number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        for i in range(len(nums)):
+            nums[i] = str(nums[i])
+       
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] + nums[j] < nums[j] + nums[i]:
+                    nums[i], nums[j] = nums[j] , nums[i]
+
+        result = "".join(nums)
+        
+        return "0" if result[0] == "0" else result
