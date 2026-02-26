d374add443c75d8f4d955ab2f9cb4b4bfbd5173f
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2026-02-26 19:21:06 +0300

    Create C. Number of Segments with Small Sum.py

---
+++ C. Number of Segments with Small Sum.py
@@ -0,0 +1,20 @@
+t , n = map(int, input().split())
+arr = list(map(int, input().split()))
+
+summ = 0
+c = 0
+left = 0
+
+for r in range(t):
+    
+    summ += arr[r]
+
+    while summ > n:
+        summ -= arr[left]
+        left += 1
+
+    c += (r - left + 1)
+
+print(c)
+
+
